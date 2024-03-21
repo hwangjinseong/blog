adcddab406f2840276c33b7f1478fa3a7e2bab2f
hwangjinseong <imhjs2023@gmail.com>
2024-03-21 11:07:41 +0900

    setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import { css } from "../../styled-system/css";
+
 export default function Home() {
   return (
     <>
-      
+    <div className={css({
+      color: 'red',
+      fontSize: '20px'
+    })}>
+
+    </div>
     </>
   );
 }
