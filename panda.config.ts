27f2c607593f826d8226c942edc352478ac9b3f9
hwangjinseong <imhjs2023@gmail.com>
2024-03-22 23:20:38 +0900

    fix : panda include

--- panda.config.ts
+++ panda.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "@pandacss/dev";
+import { defineConfig, defineRecipe } from "@pandacss/dev";
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  include: ["./src/**/*.{js,jsx,ts,tsx}"],
 
   // Files to exclude
   exclude: [],
