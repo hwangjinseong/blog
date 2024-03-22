466571cb53a543c7ba9972c4c2375470da9e7e57
hwangjinseong <imhjs2023@gmail.com>
2024-03-22 22:59:04 +0900

    feat : panda patterns setting

--- panda.config.ts
+++ panda.config.ts
@@ -17,4 +17,38 @@ export default defineConfig({
 
   // The output directory for your css system
   outdir: "styled-system",
+
+  patterns: {
+    extend: {
+      scrollable: {
+        description: "A container that allows for scrolling",
+        defaultValues: {
+          direction: "vertical",
+          hideScrollbar: true,
+        },
+        properties: {
+          // The direction of the scroll
+          direction: { type: "enum", value: ["horizontal", "vertical"] },
+          // Whether to hide the scrollbar
+          hideScrollbar: { type: "boolean" },
+        },
+        // disallow the `overflow` property (in TypeScript)
+        blocklist: ["overflow"],
+        transform(props) {
+          const { direction, hideScrollbar, ...rest } = props;
+          return {
+            overflow: "auto",
+            height: direction === "horizontal" ? "100%" : "auto",
+            width: direction === "vertical" ? "100%" : "auto",
+            scrollbarWidth: hideScrollbar ? "none" : "auto",
+            WebkitOverflowScrolling: "touch",
+            "&::-webkit-scrollbar": {
+              display: hideScrollbar ? "none" : "auto",
+            },
+            ...rest,
+          };
+        },
+      },
+    },
+  },
 });
