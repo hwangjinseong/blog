6653a4b840e866584303e13d61debffbab812280
hwangjinseong <imhjs2023@gmail.com>
2024-03-24 00:51:51 +0900

    fix : Header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,23 +1,43 @@
+import { $theme } from "@/shared/atoms/$theme";
+import { ThemeToggleButton } from "@/shared/components/ThemeToggleButton";
+import { css } from "@/styled-system/css";
 import { hstack } from "@/styled-system/patterns";
 import { FaGithub } from "react-icons/fa";
 import { FiSun } from "react-icons/fi";
+import { IoIosSearch } from "react-icons/io";
+import { useRecoilState } from "recoil";
 
 function Header() {
+  const icon = css({
+    fontSize: "2xl",
+    cursor: "pointer",
+  });
   return (
     <div
       className={hstack({
         justifyContent: "space-between",
-        p: 10,
+        px: 100,
+        py: 4,
+        borderBottom: "1px solid #eaeaea",
       })}
     >
-      <h1>blog</h1>
-      <div
-        className={hstack({
-          gap: 2,
+      <p
+        className={css({
+          fontSize: "xl",
+          fontWeight: "bold",
         })}
       >
-        <FaGithub />
-        <FiSun />
+        blog
+      </p>
+
+      <div
+        className={hstack({
+          gap: 6,
+        })}
+      >
+        <IoIosSearch className={icon} />
+        <FaGithub className={icon} />
+        <ThemeToggleButton />
       </div>
     </div>
   );
