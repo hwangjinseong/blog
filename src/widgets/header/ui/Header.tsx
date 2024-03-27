178e50080dabeeffceae5559653e8d5efa42f81b
hwangjinseong <imhjs2023@gmail.com>
2024-03-27 18:08:28 +0900

    feat : Header 다크모드 적용

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,26 +1,30 @@
 import { GithubButton } from "@/shared/components/GithubButton";
 import { SearchButton } from "@/shared/components/SearchButton";
 import { ThemeToggleButton } from "@/shared/components/ThemeToggleButton";
+import { useTheme } from "@/shared/hooks";
 import { css } from "@/styled-system/css";
 import { hstack } from "@/styled-system/patterns";
 
-const headerContainer = hstack({
-  justifyContent: "space-between",
-  px: 100,
-  py: 4,
-  borderBottom: "1px solid #eaeaea",
-});
-
-const title = css({
-  fontSize: "xl",
-  fontWeight: "bold",
-});
-
-const iconContainer = hstack({
-  gap: 6,
-});
-
 function Header() {
+  const { theme } = useTheme();
+
+  const headerContainer = hstack({
+    justifyContent: "space-between",
+    px: 200,
+    py: 4,
+    borderBottom: "1px solid #eaeaea",
+    backgroundColor: theme === "light" ? "white" : "black",
+  });
+
+  const title = css({
+    fontSize: "xl",
+    fontWeight: "bold",
+    color: theme === "light" ? "black" : "white",
+  });
+
+  const iconContainer = hstack({
+    gap: 6,
+  });
   return (
     <div className={headerContainer}>
       <p className={title}>blog</p>
