844a376a83d7a66a5b7805ccf776daf6601faa6a
hwangjinseong <imhjs2023@gmail.com>
2024-04-04 17:35:20 +0900

    style : ThemeToggleButton

--- src/shared/components/ThemeToggleButton/ui/ThemeToggleButton.tsx
+++ src/shared/components/ThemeToggleButton/ui/ThemeToggleButton.tsx
@@ -1,22 +1,37 @@
 import { useTheme } from "@/shared/hooks";
 import { css } from "@/styled-system/css";
+import { hstack } from "@/styled-system/patterns";
+import { BsFillSunFill } from "react-icons/bs";
 import { FiSun } from "react-icons/fi";
 import { IoMoon } from "react-icons/io5";
+import { PiSunFill } from "react-icons/pi";
+import { RiSunFill } from "react-icons/ri";
 
 function ThemeToggleButton() {
   const { theme, handleClickTheme } = useTheme();
 
-  const iconStyle = css({
-    fontSize: "16px",
-    sm: { fontSize: "20px" },
+  const buttonStyle = hstack({
+    width: "36px",
+    height: "36px",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "0.5rem",
     cursor: "pointer",
-    color: theme === "light" ? "black" : "white",
+    _hover: {
+      bg: theme === "light" ? "gray.100" : "gray.700",
+    },
+  });
+
+  const iconStyle = css({
+    fontSize: "18px",
+    sm: { fontSize: "24px" },
+    color: theme === "light" ? "yellow.300" : "white",
   });
 
   return (
-    <button onClick={handleClickTheme}>
+    <button onClick={handleClickTheme} className={buttonStyle}>
       {theme === "light" ? (
-        <FiSun className={iconStyle} />
+        <RiSunFill className={iconStyle} />
       ) : (
         <IoMoon className={iconStyle} />
       )}
